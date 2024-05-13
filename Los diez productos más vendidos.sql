6e81b9542e3bd2f14d8e5cfaa3bac1622157ca83
Ingrith Barbosa <iv.barbosa@uniandes.edu.co>
2024-05-13 15:43:13 -0500

    Entregables

--- Los diez productos más vendidos.sql
+++
@@ -1,7 +0,0 @@
-#Los diez productos más vendidos
-SELECT f.StockItemID, s.StockItemName, SUM(f.Quantity) AS TotalQuantity
-FROM `lab3bi.DatosLab3.Filter_OrderLines_Columns` f
-JOIN `lab3bi.DatosLab3.Generate_StockItems_ID` s ON f.StockItemID = s.StockItemID
-GROUP BY f.StockItemID, s.StockItemName
-ORDER BY TotalQuantity DESC
-LIMIT 10;
--- Mes con mayor cantidad de productos vendidos.sql
+++
@@ -1,7 +0,0 @@
-#Mes con mayor cantidad de productos vendidos
-SELECT s.Month
-FROM `lab3bi.DatosLab3.Filter_OrderLines_Columns` f
-JOIN `lab3bi.DatosLab3.Generate_Dates_ID` s ON f.DateID = s.DateID
-GROUP BY s.Month
-ORDER BY SUM(f.Quantity) DESC
-LIMIT 1;
